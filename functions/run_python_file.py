28dfe7cfcfd475f02ab3a3e2ca983a6e0a79924d
Noah <npot0222@gmail.com>
2025-11-10 08:46:08 -0700

    Added run_python_file.py and modifed some files

---
+++ functions/run_python_file.py
@@ -0,0 +1,42 @@
+import os
+import subprocess
+import sys
+def run_python_file(working_directory, file_path, args=[]):
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+    
+    
+    if not abs_file_path.startswith(abs_working_dir):
+        return f'Error: "{file_path}" is not in the working directory'
+    
+    if not os.path.isfile(abs_file_path):
+        return f'Error: "{file_path}" is not a file'
+    
+    if not file_path.endswith('.py'):
+        return f'Error: "{file_path}" is not a Python file.'
+    
+    python_executable = sys.executable #makes the program compatiable across OS's
+
+    try:
+        final_args = [python_executable, file_path]
+        final_args.extend(args)
+        output = subprocess.run(
+            final_args, 
+            cwd=abs_working_dir, 
+            timeout=30, 
+            capture_output=True)
+        
+        final_string = f'''
+        STDOUT: {output.stdout} 
+        STDERR: {output.stderr}
+        '''
+
+        if output.stdout == '' and output.stderr == '':
+            final_string = 'No output produced.\n'
+
+        if output.returncode != 0:
+            final_string += f'Process exited with code {output.returncode}'
+
+        return final_string
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
--- main.py
+++ main.py
@@ -3,13 +3,23 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 import sys
-from functions.get_files_info import get_files_info
+from functions.get_files_info import get_files_info, schema_get_files_info
 
 load_dotenv()
 def main():
     api_key = os.environ.get('GEMINI_API_KEY')
     client = genai.Client(api_key=api_key)
     verbose_flag = False
+
+    system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
 
     if len(sys.argv) < 2:
         print('I need a prompt')
@@ -24,17 +34,37 @@ def main():
         types.Content(role='user', parts=[types.Part(text=prompt)]),
     ]
 
-    response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents = messages,
-    )
-
-    print(response.text)
-    if response is None or response.usage_metadata is None:
-        print('response is malformed')
+    available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
+    try:
+        response = client.models.generate_content(
+            model='gemini-2.0-flash-001', 
+            contents = messages, 
+            config=types.GenerateContentConfig(
+        tools=[available_functions], system_instruction=system_prompt
+            )
+        )
+    except Exception as e:
+        print(e)
         return
+
     if verbose_flag:
         print(f'User prompt: {prompt}')
         print(f'Prompt Tokens: {response.usage_metadata.prompt_token_count}')
         print(f'Response Tokens: {response.usage_metadata.candidates_token_count}')
+
+    if response.function_calls:#print out functions used, if any
+        for function_call_part in response.function_calls:
+            f"Calling function: {function_call_part.name}({function_call_part.args})"
+    else:#no functions used
+        print(response.text)
+    
+    if response is None or response.usage_metadata is None:
+        print('response is malformed')
+        return
+
 
 main()
